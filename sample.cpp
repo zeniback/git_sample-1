5457ff54e78fc6abaf01e60fb50388617cdce733
zeni <zeni0102@gmail.com>
2018-11-13 20:35:43 +0900

    sabuchan sample.cpp

--- sample.cpp
+++ sample.cpp
@@ -1,16 +1,23 @@
 #include <iostream>
 
 int sum(int, int);
+int multiple(int,int);
 
 int main()
 {
     std::cout << "git training" << std::endl;
     int a = 1; int b = 9;
     std::cout << sum(a, b) << std::endl;
+    std::cout << multiple(a, b) << std::endl;
     return 0;
 }
 
 int sum(int a, int b)
 {
     return a + b;
+}
+
+int multiple(int a, int b){
+
+    return a*b;
 }
